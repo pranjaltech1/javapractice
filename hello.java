2364490e83ff21445acbcc50cd8790fbd247779e
avni <pranjal.pagaravi23@gmail.com>
2025-08-21 15:38:59 +0530

    second commit

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello{
     public static void main(String[] args) {
         System.out.println("hi from vs code..");
+        System.out.println("this is my second commit");
     }
 }
